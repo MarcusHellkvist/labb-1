ba2a1a5d9cad24b3e2950a0901661e3037063c44
Maco <hellqvistmarcus@gmail.com>
2021-09-02 01:46:03 +0200

    add test for input and button

--- __test__/SearchBar.test.js
+++ __test__/SearchBar.test.js
@@ -1,24 +1,34 @@
 import React from "react";
 import { fireEvent, render } from "@testing-library/react-native";
+import renderer from "react-test-renderer";
 import SearchBar from "../components/SearchBar";
 import Home from "../screens/Home";
 import { expect, it } from "@jest/globals";
 
-it("renders default elements", () => {
+it("renders correctly", () => {
+  renderer.create(<SearchBar />);
+});
+
+it("important elements exists", () => {
   const { getByTestId, getByPlaceholderText } = render(<SearchBar />);
   getByTestId("searchButton");
   getByPlaceholderText("Search Location");
 });
 
-it("Check callback function for search button", () => {
-  const onPress = jest.fn();
-  const testID = "searchButton";
-  const { getByText } = render(<Home />);
-
-  const { getByTestId } = render(<SearchBar clickHandler={onPress} />);
-
-  const searchButton = getByTestId(testID);
-
+it("Check callback clickhandler function for search button", () => {
+  const clickHandlerMock = jest.fn();
+  const { getByTestId } = render(<SearchBar clickHandler={clickHandlerMock} />);
+  const searchButton = getByTestId("searchButton");
   fireEvent.press(searchButton);
-  expect(onPress).toHaveBeenCalledTimes(1);
+  expect(clickHandlerMock).toHaveBeenCalledTimes(1);
+});
+
+it("Check callback updateSearchBar function for input", () => {
+  const updateSearchBarMock = jest.fn();
+  const { getByPlaceholderText } = render(
+    <SearchBar updateSearchBar={updateSearchBarMock} />
+  );
+  const input = getByPlaceholderText("Search Location", "London");
+  fireEvent.changeText(input);
+  expect(updateSearchBarMock).toHaveBeenCalled();
 });
